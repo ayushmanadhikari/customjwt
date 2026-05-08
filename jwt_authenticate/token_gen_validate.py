0d46bdbc375af353417cbe0cfd005ed681c801f3
Ayushman Adhikari <unixparadoxfb@gmail.com>
2026-05-08 11:15:35 +0545

    created access and refresh token generation functions

--- jwt_authenticate/token_gen_validate.py
+++ jwt_authenticate/token_gen_validate.py
@@ -1,4 +1,4 @@
-import PyJWT as jwt
+import jwt
 from django.conf import settings
 from datetime import timedelta, datetime
 from rest_framework.response import Response
@@ -6,22 +6,20 @@ from rest_framework.response import Response
 
 def generate_access_token(user):
     payload = {
-        'user_id': user.id, 
+        'user_id': user.id,
         'user_email': user.email,
-        'exp': datetime.now() + timedelta(minutes=15), 
+        'exp': datetime.now() + timedelta(minutes=15),
         'iat': datetime.now(),
         'type': 'access'
-
     }
-    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HSA256')
 
 
 def generate_refresh_token(user):
     payload = {
         'user_id': user.id,
         'exp': datetime.now() + timedelta(days=7),
-        'iat':  datetime.now(),
+        'iat': datetime.now(),
         'type': 'refresh'
     }
-    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HSA256')
