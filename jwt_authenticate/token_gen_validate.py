93d5e6be0fa3affdf00c580db1aa5f4578fe10f3
Ayushman Adhikari <unixparadoxfb@gmail.com>
2026-05-08 11:05:44 +0545

    first commit

---
+++ jwt_authenticate/token_gen_validate.py
@@ -0,0 +1,27 @@
+import PyJWT as jwt
+from django.conf import settings
+from datetime import timedelta, datetime
+from rest_framework.response import Response
+
+
+def generate_access_token(user):
+    payload = {
+        'user_id': user.id, 
+        'user_email': user.email,
+        'exp': datetime.now() + timedelta(minutes=15), 
+        'iat': datetime.now(),
+        'type': 'access'
+
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+
+
+def generate_refresh_token(user):
+    payload = {
+        'user_id': user.id,
+        'exp': datetime.now() + timedelta(days=7),
+        'iat':  datetime.now(),
+        'type': 'refresh'
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
